4affb8b9c5af3aee8c5362c1ad49b85bbbb07f31
anilyavas <anil.yavas99@gmail.com>
2024-12-29 19:36:59 +0300

    News Page back button enabled

--- components/News.tsx
+++ components/News.tsx
@@ -1,35 +1,43 @@
 import { FontAwesome } from '@expo/vector-icons';
 import { Stack } from 'expo-router';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { Dimensions, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default function News() {
-  const [url, setUrl] = useState('https://www.formula1.com/en/latest/all.html');
+  const webviewRef = useRef(null);
+  const [canGoBack, setCanGoBack] = useState(false);
 
-  const reloadPage = () => {
-    setUrl('https://www.formula1.com/en/latest/all.html');
+  const handleNavigationStateChange = (navState) => {
+    setCanGoBack(navState.canGoBack); // Update back button state
+  };
+
+  const goBack = () => {
+    if (webviewRef.current && canGoBack) {
+      webviewRef.current.goBack(); // Go to the previous page
+    }
   };
 
   return (
-    <View className="flex-1">
+    <View style={{ flex: 1 }}>
       <Stack.Screen
         options={{
           headerLeft: () => (
             <FontAwesome
               name="arrow-left"
               size={25}
-              color="white"
+              color={canGoBack ? 'white' : 'gray'} // Disable if can't go back
               style={{ paddingLeft: 20 }}
-              onPress={reloadPage}
+              onPress={goBack}
             />
           ),
         }}
       />
       <WebView
-        key={url}
-        source={{ uri: url }}
+        ref={webviewRef}
+        source={{ uri: 'https://www.formula1.com/en/latest/all.html' }}
         style={{ width: Dimensions.get('window').width, height: Dimensions.get('window').height }}
+        onNavigationStateChange={handleNavigationStateChange} // Track navigation state
       />
     </View>
   );
