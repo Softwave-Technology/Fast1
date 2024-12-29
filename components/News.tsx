192e6f25c535e51e7eba6ef988cedefb3f559036
anilyavas <anil.yavas99@gmail.com>
2024-12-29 19:38:25 +0300

    TS error fix

--- components/News.tsx
+++ components/News.tsx
@@ -5,16 +5,16 @@ import { Dimensions, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default function News() {
-  const webviewRef = useRef(null);
+  const webviewRef = useRef<WebView>(null);
   const [canGoBack, setCanGoBack] = useState(false);
 
-  const handleNavigationStateChange = (navState) => {
-    setCanGoBack(navState.canGoBack); // Update back button state
+  const handleNavigationStateChange = (navState: any) => {
+    setCanGoBack(navState.canGoBack);
   };
 
   const goBack = () => {
     if (webviewRef.current && canGoBack) {
-      webviewRef.current.goBack(); // Go to the previous page
+      webviewRef.current.goBack();
     }
   };
 
@@ -26,7 +26,7 @@ export default function News() {
             <FontAwesome
               name="arrow-left"
               size={25}
-              color={canGoBack ? 'white' : 'gray'} // Disable if can't go back
+              color={canGoBack ? 'white' : 'gray'}
               style={{ paddingLeft: 20 }}
               onPress={goBack}
             />
@@ -37,7 +37,7 @@ export default function News() {
         ref={webviewRef}
         source={{ uri: 'https://www.formula1.com/en/latest/all.html' }}
         style={{ width: Dimensions.get('window').width, height: Dimensions.get('window').height }}
-        onNavigationStateChange={handleNavigationStateChange} // Track navigation state
+        onNavigationStateChange={handleNavigationStateChange}
       />
     </View>
   );
